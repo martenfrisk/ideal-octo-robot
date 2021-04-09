07796bc0a96774caf21bf11aafed451de6bbecde
Marten <marten.frisk@gmail.com>
2021-04-09 11:37:27 +0200

    Move and rename global types file

--- client.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "$crown-components" {
-  type Modules = { [key: string]: () => Promise<any> };
-  export default {} as Modules;
-}
-
-declare module "*.svelte?mount" {}
-declare module "*.svelte?mount?eager" {}
-declare module "*.svelte?mount?lazy" {}
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare module "$crown-components" {
+  type Modules = { [key: string]: () => Promise<any> };
+  export default {} as Modules;
+}
+
+declare module "*.svelte!mount" {}
+declare module "*.svelte!mount!eager" {}
+declare module "*.svelte!mount!lazy" {}
