6c32627db510bd35faa7e82b84c0375894a9a5b9
Marten <marten.frisk@gmail.com>
2021-04-09 16:44:47 +0200

    Add ssr check with additional client loader function

--- src/vite-plugin-svelte-partial-hydration/index.ts
+++ src/vite-plugin-svelte-partial-hydration/index.ts
@@ -1,38 +1,55 @@
 export default function vitePluginSveltePartialHydration() {
-  const mountFilter = /^(.+\.svelte)!mount(?:=(\w+))?$/;
+  const mountFilter = /^(.+\.svelte)!mount(?:=(\w+))?$/
+  const clientFilter = /^\$crown-components$/
 
   return {
-    enforce: 'pre',
-    name: "vite-plugin-svelte-partial-hydration", 
-    async resolveId(id, importer) {
+    enforce: "pre",
+    name: "vite-plugin-svelte-partial-hydration",
+    async resolveId(id, importer, ssr) {
       // console.log(id)
-      let matches = id.match(mountFilter)
-      
-      if (matches) {
-        const realPath = matches[1]
-        const args = matches[2] || ""
+      if (ssr) {
+        let matches = id.match(mountFilter)
+        if (matches) {
+          const realPath = matches[1]
+          const args = matches[2] || ""
 
-        const resolution = await this.resolve(realPath, importer, { skipSelf: true })
-        console.log("-------- resolve>", id, resolution)
-  
-        return {id: resolution.id + '!mount' + args}
+          const resolution = await this.resolve(realPath, importer, {
+            skipSelf: true,
+          })
+          console.log("-------- resolve>", id, resolution)
+
+          return { id: resolution.id + "!mount" + args }
+        }
       }
     },
-    async load(id) {
-      let matches = id.match(mountFilter)
+    async load(id, ssr) {
+      if (ssr) {
+        let matches = id.match(mountFilter)
 
-      if (matches) {
-        const realPath = matches[1]
-        const args = matches[2] || ""
-        console.log("***** matches ", matches)
-        
-        console.log("-------- load>", realPath, args)
-        return {
-          code: `
-    import Component from "${realPath}";
-    import { patch } from "C:/Users/Marten/Documents/Web/svelte-partial-vite/src/mount/utils.ts";
-    export default patch(Component, "${realPath}", "")
+        if (matches) {
+          const realPath = matches[1]
+          const args = matches[2] || ""
+
+          console.log("-------- load>", realPath, args)
+          return {
+            code: `
+          import Component from "${realPath}";
+          import { patch } from "C:/Users/Marten/Documents/Web/svelte-partial-vite/src/mount/utils.ts";
+          export default patch(Component, "${realPath}", "")
           `,
+          }
+        }
+      } else {
+        let matches = id.match(clientFilter)
+        if (matches) {
+          const realPath = matches[1]
+          const args = matches[2] || ""
+          console.log("-------- load NOT_SSR>", realPath, args)
+          return {
+            code: `
+           import("${realPath}");
+          `,
+          }
         }
       }
     },
