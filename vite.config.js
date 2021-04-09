7ea6288c90b4abfad7cd44c608dd4a5e058642a6
Marten <marten.frisk@gmail.com>
2021-04-09 11:37:42 +0200

    Add separate server plugins to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import svelte from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite"
+import svelte from "@sveltejs/vite-plugin-svelte"
+// import partial from "./src/plugin/src"
+import partial from "./src/vite-plugin-svelte-partial-hydration"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  server: {
+    plugins: [
+      svelte(),
+      partial(),
+    ],
+  },
+  plugins: [
+    svelte(),
+    partial(),
+  ],
 })
