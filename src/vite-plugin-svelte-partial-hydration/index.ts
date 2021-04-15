007e499b833b953c3b4accd9d48c7870f439cc57
Marten <marten.frisk@gmail.com>
2021-04-15 10:18:03 +0200

    Add filter for crown-components

--- src/vite-plugin-svelte-partial-hydration/index.ts
+++ src/vite-plugin-svelte-partial-hydration/index.ts
@@ -5,8 +5,14 @@ export default function vitePluginSveltePartialHydration() {
   return {
     enforce: "pre",
     name: "vite-plugin-svelte-partial-hydration",
-    async resolveId(id, importer, ssr) {
+    async resolveId(id, importer, source, ssr) {
       // console.log(id)
+      if (id.match(clientFilter)) {
+        console.log(
+          `$CROWN-COMP - RESOLVE, id: ${id}, source: ${JSON.stringify(source)}`
+        )
+        return id
+      }
       if (ssr) {
         let matches = id.match(mountFilter)
         if (matches) {
@@ -16,40 +22,42 @@ export default function vitePluginSveltePartialHydration() {
           const resolution = await this.resolve(realPath, importer, {
             skipSelf: true,
           })
-          console.log("-------- resolve>", id, resolution)
+          console.log("-------- resolve>", id, resolution.id)
 
           return { id: resolution.id + "!mount" + args }
         }
       }
     },
     async load(id, ssr) {
+      if (id.match(clientFilter)) {
+        console.log("$CROWN-COMP - LOAD", id, ssr)
+        return {
+          code: `
+            export default {
+              "./Lazy.svelte": () => import("./src/lib/Lazy.svelte")
+            };
+          `,
+        }
+      }
       if (ssr) {
         let matches = id.match(mountFilter)
+        console.log(matches)
 
         if (matches) {
           const realPath = matches[1]
           const args = matches[2] || ""
-
-          console.log("-------- load>", realPath, args)
-          return {
-            code: `
-          import Component from "${realPath}";
-          import { patch } from "C:/Users/Marten/Documents/Web/svelte-partial-vite/src/mount/utils.ts";
-          export default patch(Component, "${realPath}", "")
-          `,
-          }
-        }
-      } else {
-        let matches = id.match(clientFilter)
-        if (matches) {
-          const realPath = matches[1]
-          const args = matches[2] || ""
-          console.log("-------- load NOT_SSR>", realPath, args)
-          return {
-            code: `
-           import("${realPath}");
-          `,
-          }
+            // const resolution = await this.resolve(realPath, importer, {
+            //   skipSelf: true,
+            // })
+            // console.log("-------- load1>", id, importer)
+            console.log("-------- load>", realPath, args)
+            return {
+              code: `
+            import Component from "${realPath}";
+            import { patch } from "/src/mount/utils.ts";
+            export default patch(Component, "./Lazy.svelte", "")
+            `,
+            }
         }
       }
     },
