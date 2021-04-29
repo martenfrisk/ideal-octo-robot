06b3ff5600fdb0c507839f974d5f912466268175
Marten <marten.frisk@gmail.com>
2021-04-29 08:53:29 +0200

    Never minify (easier debugging)

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command, mode }) => {
     plugins: [partial(), svelte()],
     build: {
       manifest: true,
-      // minify: false,
+      minify: false,
       // rollupOptions: {
       //   external: !isProduction && ["/assets/manifest.json"],
       // },
