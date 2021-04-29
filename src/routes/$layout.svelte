9cf3e40520200b6f101ea2cf7c08a1b6bff15ee7
Marten <marten.frisk@gmail.com>
2021-04-29 08:52:47 +0200

    Add non-functional route pages

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,15 @@
+<nav><a href="/">Home</a><a href="/about">About</a></nav>
+
+<main>
+
+  <slot></slot>
+</main>
+
+<style>
+  main {
+    background-color: green;
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  // @ts-nocheck
+  import { SvelteComponent } from "svelte"
+  // export let name;
+  import Counter from "./lib/Counter.svelte!mount"
+  import Lazy from "./lib/Lazy.svelte!mount"
+  export let component: SvelteComponent, componentProps: SvelteAllProps
+</script>
+
+<div>
+  <h1>Hello Partial!</h1>
+  <Counter />
+  <Lazy server={Date.now()} foo="testProp" />
+  <Lazy server={Date.now()} foo="testProp" />
+  <svelte:component this={component} {...componentProps || {}} />
+</div>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+  }
+</style>
