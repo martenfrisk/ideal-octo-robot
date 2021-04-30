3e52d97a07b299024684ec12ffff03d56cc2b0c4
Marten <marten.frisk@gmail.com>
2021-04-30 10:26:46 +0200

    Add router file (unused atm)

---
+++ src/router/Route.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { RenderProps } from '.';
+  export let tree: RenderProps[];
+  const [current, ...rest] = tree;
+</script>
+
+<svelte:component this={current.component} {...current.props}>
+  {#if rest.length}
+    <svelte:self tree={rest} />
+  {/if}
+</svelte:component>
