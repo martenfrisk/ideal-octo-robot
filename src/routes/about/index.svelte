d2143e9ccb2943f965f7f5cd45c4a9d23bd16765
Marten <marten.frisk@gmail.com>
2021-04-30 10:24:16 +0200

    Add experimental module fn

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,7 +1,18 @@
+<script context="module">
+  export function get() {
+    return { myProp: 'test' }
+  } 
+</script>
+
 <script lang="ts">
   // @ts-nocheck
-import Lazy from "src/lib/Lazy.svelte!mount";
+  import Lazy from "../../lib/Lazy.svelte";
+  export let myProp;
 </script>
+
 <a href="/">Home</a>
 <p>About page</p>
+
+My prop: {myProp}
+
 <Lazy foo="aboutProp" date={Date.now()} />
