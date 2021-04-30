1ff9b1f5f2ec13ddbe57b346264ecb82e1151dc9
Marten <marten.frisk@gmail.com>
2021-04-30 10:24:46 +0200

    Import router functions in entry server

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,11 +1,35 @@
+import { routes, getMatchingRoutes } from './router';
+import type { SvelteComponent } from 'svelte'
+import type { RenderProps } from './router';
+
 import App from './App.svelte'
+import Layout from './routes/$layout.svelte'
 
 export async function render(
-  url: string,
+  path: string,
   manifest: any
-): Promise<[string, string]> {
+): Promise<any> {
+  const components = getMatchingRoutes(path)
+  // const components: RenderProps[] = [
+  //   { component: Layout, props: {}},
+  //   { component: route, props: {}}
+  // ]
+
+  // const promises = components.map(component => {
+  //   if (component.get) return component.get(/* args */)
+  // })
+
+  // const props = await Promise.allSettled(promises)
+
+  // Merge components and props
+
+  // console.log(Layout.render())
+  console.log('entry-server components', components)
+  
   // @ts-ignore
-  return App.render({
-    name: 'world'
-  });
+  const { html, css, head } = App.render({
+    components
+  }) 
+  
+  return { html, css, head }
 }
