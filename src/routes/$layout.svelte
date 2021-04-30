3762a347970f25081ed4d588b85fbb246efd17b7
Marten <marten.frisk@gmail.com>
2021-04-30 10:25:45 +0200

    Add layout boilerplate

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,15 +1,23 @@
-<nav><a href="/">Home</a><a href="/about">About</a></nav>
+<nav><a href="/">Home</a><a href="/about/">About</a></nav>
 
 <main>
-
-  <slot></slot>
+  <slot />
 </main>
 
 <style>
+  nav {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
   main {
-    background-color: green;
+    background-color: #fff;
     text-align: center;
     padding: 1em;
     margin: 0 auto;
+  }
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 </style>
