e4ad2d6bf1553de31b8a7e44711f2c37284c705b
Marten <marten.frisk@gmail.com>
2021-05-03 14:13:43 +0200

    Remove unused import
    Also remove console.logs

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,35 +1,35 @@
-import { routes, getMatchingRoutes } from './router';
-import type { SvelteComponent } from 'svelte'
-import type { RenderProps } from './router';
+import { getMatchingRoutes } from "./router"
 
-import App from './App.svelte'
-import Layout from './routes/$layout.svelte'
+import App from "./App.svelte"
 
-export async function render(
-  path: string,
-  manifest: any
-): Promise<any> {
-  const components = getMatchingRoutes(path)
-  // const components: RenderProps[] = [
-  //   { component: Layout, props: {}},
-  //   { component: route, props: {}}
-  // ]
+export async function render(path: string, manifest: any): Promise<any> {
+    const components = getMatchingRoutes(path)
 
-  // const promises = components.map(component => {
-  //   if (component.get) return component.get(/* args */)
-  // })
+    if (components) {
+      // const components: RenderProps[] = [
+      //   { component: Layout, props: {}},
+      //   { component: route, props: {}}
+      // ]
 
-  // const props = await Promise.allSettled(promises)
+      // const promises = components.map((component) => {
+      //   if (component.get) return component.get(/* args */)
+      // })
 
-  // Merge components and props
+      // const props = await Promise.allSettled(promises)
 
-  // console.log(Layout.render())
-  console.log('entry-server components', components)
-  
-  // @ts-ignore
-  const { html, css, head } = App.render({
-    components
-  }) 
-  
-  return { html, css, head }
+      // Merge components and props
+
+      // console.log(Layout.render())
+      // @ts-ignore
+      const { html, css, head } = App.render({
+        components,
+      })
+
+      // console.log({ html })
+      // console.log({ css })
+      // console.log({ head })
+
+      return { html, css, head }
+    }
+    return null
 }
