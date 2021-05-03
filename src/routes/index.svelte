50820b81725efd6704d186fc44b294f5b2c8e285
Marten <marten.frisk@gmail.com>
2021-05-03 17:07:57 +0200

    Move h1 style to layout file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,15 +16,7 @@
 </div>
 
 <style>
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
+
 
   @media (min-width: 480px) {
     h1 {
