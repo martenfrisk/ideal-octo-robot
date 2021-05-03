a55bea435571ffb4ad5c5fedd746449d57093a31
Marten <marten.frisk@gmail.com>
2021-05-03 14:13:16 +0200

    Add recursive svelte comp to App
    To support nested layouts

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import type { RenderProps } from "./router"
   export let components: RenderProps[]
-  const [current, ...rest] = components
-  console.log(components)
-  
+  let current, rest
+  if (components) [current, ...rest] = components
 </script>
 
-<svelte:component this={current.component} {...current.props}>
-  <!-- {#if components.length > 1}
-    <svelte:self components={rest} />
-  {/if} -->
+<svelte:component this={current}>
+  {#if components.length}
+      <svelte:self components={rest} />
+    {/if}
 </svelte:component>
