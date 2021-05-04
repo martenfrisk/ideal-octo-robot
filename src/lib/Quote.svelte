76eb65f95f95c7eb7d99793ce1cbbe65b5ea083d
Marten <marten.frisk@gmail.com>
2021-05-04 18:30:10 +0200

    Add svelte file to test fetching external api
    Doesn't quite work yet.

---
+++ src/lib/Quote.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  export async function get() {
+    const response = await this.fetch('https://zenquotes.io/api/random')
+    const data = await response.json()
+    return { data }
+  }
+</script>
+
+<script>
+  export let data
+</script>
+
+<p>Quote goes here:</p>
+{#if data}
+<p>{data}</p>
+{:else}
+<p>No quote 😔</p>
+{/if}
