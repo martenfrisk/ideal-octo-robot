aa6446d2a37b5cb5fa630a3b1d3974a6cf9c44b4
Marten <marten.frisk@gmail.com>
2021-05-04 18:31:04 +0200

    Enable context-module in about page
    This is just a test to return a static prop.
    Gets picked up by the entry-server but can't
    seem to pass it to the rendered markup.
    Needs fixing.

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,17 +1,22 @@
-<!-- <script context="module">
+<script context="module">
   export function get() {
     return { myProp: 'test' }
-  } 
-</script> -->
+  }
+</script>
 
 <script lang="ts">
   // @ts-nocheck
   import Lazy from "../../lib/Lazy.svelte!mount";
+  import Quote from "../../lib/Quote.svelte!mount";
+  export let myProp
 </script>
 
 <h1>About page</h1>
+<p>{myProp}</p>
+
 <p>This is the about page. The style of this paragraph comes from this page's svelte file.</p>
 
+<Quote />
 <Lazy foo="aboutProp" server={Date.now()} />
 
 <style>
