913c2935669b22e57cb06f86ce4b872001c8f74a
Marten <marten.frisk@gmail.com>
2021-05-05 14:18:49 +0200

    Call function and pass as props
    Doesn't work yet

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -6,31 +6,34 @@ export async function render(path: string, manifest: any): Promise<any> {
   const components = getMatchingRoutes(path)
 
   if (components) {
-    const componentsDefault = components.map((x) => x.default)
-
-    const promises = components.map((component) => {
-      if (component.get) {
-        // console.log(component.get())
-
-        return component.get()
-      }
-    })
-
-    const props = await Promise.allSettled(promises).then((results) =>
-      results.map((x) => {
-        // console.log({x})
-
-        if (x.status === "fulfilled" && !!x.value) return x.value
-      })
-    )
     // Merge components and props
-
-    // @ts-ignore
-    const { html, css, head } = App.render({
-      ...props,
-      components: componentsDefault,
+    const componentsDefault = components.map((component) => {
+      // if component has get(), run it and add its response as props
+      if (component.getFn) {
+        return {
+          props: (component.getFn()) || {},
+          component: component.default,
+        }
+      } 
+      // else just include the component
+        return {
+          component: component.default,
+        }
     })
-    return { html, css, head }
+
+    // const promises = components.map((component) => {
+    //   if (component.get) return component.get()
+    // })
+
+    // const props = await Promise.allSettled(promises).then((data) =>
+    //   data.filter((x) => x.status == "fulfilled")
+    // )
+    
+      // @ts-ignore
+      const { html, css, head } = App.render({
+        components: componentsDefault,
+      })
+      return { html, css, head }
   }
   return null
 }
