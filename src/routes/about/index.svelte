a0dfaef4038bd71f4dcafeb44691c3970955c13d
Marten <marten.frisk@gmail.com>
2021-05-05 14:18:35 +0200

    Move SS function to page file

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,22 +1,40 @@
 <script context="module">
-  export function get() {
-    return { myProp: 'test' }
+  import fetch from "isomorphic-unfetch"
+  export async function getFn() {
+    const response = await fetch("https://zenquotes.io/api/random")
+    const data = await response.json()
+    return { data }
   }
 </script>
 
 <script lang="ts">
   // @ts-nocheck
-  import Lazy from "../../lib/Lazy.svelte!mount";
-  import Quote from "../../lib/Quote.svelte!mount";
-  export let myProp
+  export let data: {
+    q: string
+    a: string
+    h: string
+  }
+  console.log("about: ", { data })
+
+  import Lazy from "../../lib/Lazy.svelte!mount"
+  // import Quote from "../../lib/Quote.svelte!mount"
 </script>
 
 <h1>About page</h1>
-<p>{myProp}</p>
+<p>
+  {#if data}
+    {data.h}
+  {:else}
+    No prop
+  {/if}
+</p>
 
-<p>This is the about page. The style of this paragraph comes from this page's svelte file.</p>
+<p>
+  This is the about page. The style of this paragraph comes from this page's
+  svelte file.
+</p>
 
-<Quote />
+<!-- <Quote /> -->
 <Lazy foo="aboutProp" server={Date.now()} />
 
 <style>
