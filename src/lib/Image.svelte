e174a3f2d1429b61bdee93811972e95860779c60
Marten <marten.frisk@gmail.com>
2021-05-18 10:52:37 +0200

    Add image component
    With external image for testing

---
+++ src/lib/Image.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let imgsrc: string,
+    altText = "Placeholder text"
+    function newImage() {
+      const newSource = "https://picsum.photos/200/200"
+      imgsrc === newSource ? imgsrc = "https://picsum.photos/300/200" : imgsrc = newSource
+    }
+</script>
+
+<div on:click={newImage}>
+  <img src={imgsrc} alt={altText} />
+</div>
+
+<style>
+  div {
+    cursor: pointer;
+    width: 100%;
+    margin: 2rem 0;
+    display: flex;
+    justify-content: center;
+  }
+</style>
