582c6574aaaca4594e9123b953a204d4098669fc
Marten <marten.frisk@gmail.com>
2021-05-18 10:55:58 +0200

    Remove unused svelte-component in index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
   // @ts-nocheck
-  import { SvelteComponent } from "svelte"
-  // export let name;
+  import Image from "../lib/Image.svelte!mount";
   import Counter from "../lib/Counter.svelte!mount"
-  import Lazy from "../lib/Lazy.svelte!mount"
-  export let component: SvelteComponent, componentProps: SvelteAllProps
 </script>
 
 <div>
   <h1>Hello Partial!</h1>
-  <Counter />
-  <Lazy server={Date.now()} foo="testProp" />
-  <Lazy server={Date.now()} foo="testProp" />
-  <svelte:component this={component} {...componentProps || {}} />
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eveniet
+      odio alias expedita officia quasi cum aspernatur optio, quos nemo
+      reprehenderit. Quod, corporis. Necessitatibus nulla excepturi culpa modi
+      molestias minus.
+    </p>
+  </div>
+  <div class="cv">
+    <Counter />
+  </div>
+  <Image imgsrc="https://picsum.photos/300/200" />
 </div>
 
 <style>
-
-
+  .cv {
+    content-visibility: auto;
+    contain-intrinsic-size: 57px 200px;
+  }
+  div {
+    width: 100%;
+    flex-wrap: wrap;
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+  }
+  p {
+    width: 50%;
+  }
   @media (min-width: 480px) {
     h1 {
       max-width: none;
     }
-
   }
 </style>
