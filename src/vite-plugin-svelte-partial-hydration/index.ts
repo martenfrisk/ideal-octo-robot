4997679e7da9a9db439926e34a78e7281c8e57cb
Marten <marten.frisk@gmail.com>
2021-05-18 10:55:46 +0200

    Remove unused code; add static fallback

--- src/vite-plugin-svelte-partial-hydration/index.ts
+++ src/vite-plugin-svelte-partial-hydration/index.ts
@@ -20,6 +20,7 @@ export default function vitePluginSveltePartialHydration(
   { clientManifestPath }: Options = {
     clientManifestPath: "./dist/client/manifest.json",
   }
+
 ) {
   const mountFilter = /^(.+\.svelte)!mount(?:=(\w+))?$/
   const clientFilter = /^\$crown-components$/
@@ -37,16 +38,14 @@ export default function vitePluginSveltePartialHydration(
     async configResolved(config) {
       requestParser = buildIdParser(config)
     },
-    // async resolveDynamicImport() {
-
-    // },
     async resolveId(id, importer, source, ssr) {
       if (id.match(clientFilter)) {
         return id
       }
       if (ssr) {
         let matches = id.match(mountFilter)
-
+        // console.log({matches})
+        
         if (matches) {
           const realPath = matches[1]
           const args = matches[2] || ""
@@ -55,8 +54,6 @@ export default function vitePluginSveltePartialHydration(
           })
 
           if (resolution) {
-            // console.log({resolution})
-            
             return { id: resolution.id + "!mount" + args }
           }
 
@@ -65,32 +62,32 @@ export default function vitePluginSveltePartialHydration(
       }
     },
     async load(id, ssr) {
-      // console.log(`load id: ${id}`)
-
+      // console.log({id})
+      
       if (id.match(clientFilter)) {
-        let entries
 
-        // console.log("$CROWN-COMP - LOAD", id, ssr)
+        let entries
         if (server) {
           const graph = server.moduleGraph.idToModuleMap.entries()
           entries = Array.from(graph)
             .filter(([id]) => id.match(mountFilter))
             .map(([id, meta]) => [id, meta.url])
-        } 
-        // else if (clientManifestPath) {
-        //   try {
-        //     let clientManifest = require(clientManifestPath)
-        //     entries = Object.entries(clientManifest)
-        //     // @ts-ignore
-        //       .filter(([id, meta]) => meta.isDynamicEntry)
-        //       .map(([id, meta]) => [id, id])
-        //   } catch (e) {
-        //     console.error(e)
-        //   }
-        // }
-        if (entries && entries.length) {
-          // console.log({entries})
+        }
+        else if (clientManifestPath) {
+          console.log({clientManifestPath})
           
+          try {
+            let clientManifest = require(clientManifestPath)
+            entries = Object.entries(clientManifest)
+              .filter(([id, meta]: [string, any]) => meta.isDynamicEntry)
+              .map(([id, meta]) => ['/' + id, '/' + id])
+          } catch (e) {
+            console.error(e)
+          }
+        }
+        
+        if (entries && entries.length) {
+          console.log({entries})
           return {
             code: `
 export default { 
@@ -98,20 +95,22 @@ export default {
 };`,
           }
         }
-        // this is static in prod but should be dynamic
+        // static fallback, possibly remove this
         return `export default{
           '/src/lib/Counter.svelte': () =>import ('/src/lib/Counter.svelte'),
+          '/src/lib/Quote.svelte': () =>import ('/src/lib/Quote.svelte'),
+          '/src/lib/Image.svelte': () =>import ('/src/lib/Image.svelte'),
           '/src/lib/Lazy.svelte': () =>import ('/src/lib/Lazy.svelte')
         };`
       }
 
       if (ssr) {
         let matches = id.match(mountFilter)
-
+        
         if (matches) {
+          console.log('is ssr', id)
           const svelteRequest = requestParser(id, !!ssr)
           const realPath = matches[1]
-          // console.log('---- load', normalizePath(realPath))
           return {
             code: `
             import Component from "${realPath}";
